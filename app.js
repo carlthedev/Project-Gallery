5a5b3dd58174dab1381670413d8e7e29fbfff75e
carl <carl.cross@yahoo.co.uk>
2022-11-04 09:32:28 +0000

    several changes in 1 commit

---
+++ app.js
@@ -0,0 +1,49 @@
+function drop_down() {
+  let x = document.getElementById("dtls");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
+
+function drop_down2() {
+  let x = document.getElementById("dtls2");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
+
+function drop_down3() {
+  let x = document.getElementById("dtls3");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
+
+function drop_down4() {
+  let x = document.getElementById("dtls4");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
+
+function drop_down5() {
+  let x = document.getElementById("dtls5");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
